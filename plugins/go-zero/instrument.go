4ccb060a29482004c30bcb68de3a23f9b31bd738
wangp <wangp@aeroht.com>
2023-09-12 17:45:11 +0800

    backup

---
+++ plugins/go-zero/instrument.go
@@ -0,0 +1,67 @@
+/*
+ * @Description:
+ * @version: 1.0.0
+ * @Author: wangp@aeroht.com
+ * @Date: 2023-09-12 15:40:20
+ */
+
+package gozero
+
+import (
+	"embed"
+
+	"github.com/apache/skywalking-go/plugins/core/instrument"
+)
+
+//go:embed *
+var fs embed.FS
+
+type Instrument struct {
+}
+
+func NewInstrument() *Instrument {
+	return &Instrument{}
+}
+
+func (i *Instrument) Name() string {
+	return "go-zero"
+}
+
+func (i *Instrument) BasePackage() string {
+	return "github.com/zeromicro/go-zero"
+}
+
+func (i *Instrument) VersionChecker(version string) bool {
+	return true
+}
+
+func (i *Instrument) Points() []*instrument.Point {
+	return []*instrument.Point{
+		// http transport
+		{
+			PackagePath: "rest",
+			At:          instrument.NewStructEnhance("Server"),
+		},
+		{
+			PackagePath: "rest",
+			At: instrument.NewStaticMethodEnhance("NewServer",
+				instrument.WithArgsCount(2), 
+					instrument.WithArgType(0, "RestConf"), instrument.WithArgType(1, "...RunOption"),
+				instrument.WithResultCount(2), 
+					instrument.WithResultType(0, "*Server"), instrument.WithResultType(1, "error")),
+			Interceptor: "NewServerInterceptor",
+		},
+		{
+			PackagePath: "rest",
+			At: instrument.NewMethodEnhance("*Server", "Use", 
+				instrument.WithArgsCount(1),
+				instrument.WithArgType(0, "Middleware"),
+				instrument.WithResultCount(0)),
+			Interceptor: "ServerMiddlewareInterceptor",
+		},
+	}
+}
+
+func (i *Instrument) FS() *embed.FS {
+	return &fs
+}
---
+++ plugins/go-zero/transport/rest/server.go
@@ -0,0 +1,41 @@
+/*
+ * @Description:
+ * @version: 1.0.0
+ * @Author: wangp@aeroht.com
+ * @Date: 2023-09-12 17:25:52
+ */
+package rest
+
+import (
+	"github.com/apache/skywalking-go/plugins/core/operator"
+	"github.com/go-kratos/kratos/v2/transport/http"
+
+	"github.com/zeromicro/go-zero/rest"
+
+	"github.com/apache/skywalking-go/plugins/core/tracing"
+)
+
+var ignoreServerMiddlewareKey = "ignoreServerMiddleware"
+
+type NewServerInterceptor struct {
+}
+
+func (n *NewServerInterceptor) BeforeInvoke(invocation operator.Invocation) error {
+	return nil
+}
+
+func (n *NewServerInterceptor) AfterInvoke(invocation operator.Invocation, results ...interface{}) error {
+	serverEnhanced, ok := results[0].(operator.EnhancedInstance)
+	if !ok || serverEnhanced.GetSkyWalkingDynamicField() == true {
+		return nil
+	}
+	server, ok := results[0].(*rest.Server)
+	if !ok {
+		return nil
+	}
+	// adding the middleware to the server
+	tracing.SetRuntimeContextValue(ignoreServerMiddlewareKey, true)
+	http.Middleware(serverMiddleware)(server)
+	serverEnhanced.SetSkyWalkingDynamicField(true)
+	return nil
+}
